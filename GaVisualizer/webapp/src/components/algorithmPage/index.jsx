09c492bbe3a475d8a3904146c996d028dd693180
Ruslan Rasulov <ruslanrasulov31@gmail.com>
2019-02-24 22:21:56 +0400

    add algorithm functionality

--- GaVisualizer/webapp/src/components/algorithmPage/index.jsx
+++ GaVisualizer/webapp/src/components/algorithmPage/index.jsx
@@ -1,9 +1,38 @@
 import React, { Component } from 'react';
 import AlgorithmView from '../algorithmView';
+import './_styles.scss';
 
 class AlgorithmPage extends Component {
+    constuctor(props) {
+        this.super(props);
+    }
+
+    state = {
+        algorithmCount: 1
+    }
+
+    onAddAlgorithm = () => {
+        this.setState((prevState) => ({
+            algorithmCount: prevState.algorithmCount + 1
+        }));
+    }
+
+    renderAlgorithms = () => {
+        const elements = [];
+        const { algorithmCount } = this.state;
+
+        for (let i = 0; i < algorithmCount; i++) {
+            elements.push(<AlgorithmView key={i}/>);
+        }
+
+        return elements;
+    }
+
     render = () => (
-        <AlgorithmView />
+        <div>
+            <div className='btn btn-add' onClick={this.onAddAlgorithm}>Add a new algorithm</div>
+            {this.renderAlgorithms()}
+        </div>
     );
 }
 
