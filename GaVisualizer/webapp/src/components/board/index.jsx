64a2412f94d1eb1d7cb22aea99b38e0031ae37f3
Ruslan Rasulov <ruslanrasulov31@gmail.com>
2019-05-25 23:49:01 +0400

    implemented crossover animation

--- GaVisualizer/webapp/src/components/board/index.jsx
+++ GaVisualizer/webapp/src/components/board/index.jsx
@@ -75,8 +75,13 @@ class Board extends Component {
 
         for (let i = 0; i < 5; i++) { //TODO: For temproary purpose
             for (let j = 0; j < 5; j++) {
+                if (i === j && this.state.hide) {
+                    continue;
+                }
+
                 const { x, y } = this.calculatePosition(i, j, cellWidth, cellHeight);
                 this.drawCell(ctx, cells[i][j], x, y, cellWidth, cellHeight);
+                this.drawCellValues(ctx, cells[i][j], x, y);
                 // let rectFillStyle;
                 // let textFillStyle;
 
@@ -187,7 +192,7 @@ class Board extends Component {
 
         if (!this.props.generation.cells) return;
         const { cellWidth, cellHeight } = this.getBoardSize();
-
+        ctx.save();
         for (let i = 0; i < 5; i++) {
             this.fadeElement(ctx, i, i, cellWidth, cellHeight);
         }
@@ -222,26 +227,57 @@ class Board extends Component {
         ctx.clearRect(x - 1, y - 1, cellWidth + 2, cellHeight + 2);
 
         this.drawCell(ctx, this.props.generation.cells[i][j], x, y, cellWidth, cellHeight);
+        this.drawCellValues(ctx, this.props.generation.cells[i][j], x, y);
 
         if (step === 100) {
-            ctx.restore();
+            ctx.globalAlpha = 1;
             return;
         }
 
         requestAnimationFrame(() => this.fadeElement(ctx, i, j, cellWidth, cellHeight, step + 1));
     }
 
-    drawCell = (ctx, cell, x, y, cellWidth, cellHeight) => {
-        let rectFillStyle;
+    drawCellValues = (ctx, cell, x, y, gene = null) => {
         let textFillStyle;
 
         if (cell.elementType === 0) { //bacterium
-            rectFillStyle = '#00ff00';
             textFillStyle = '#000000';
         }
         else if (cell.elementType === 1){ //virus
-            rectFillStyle = '#ff0000';
             textFillStyle = '#ffffff';
+        }
+
+        if (!this.props.editMode) {
+            const textX = x  + 6;
+            const textY = y + 15;
+            
+            ctx.fillStyle = textFillStyle;
+            
+            if (gene === null) {
+                const age = `Age: ${cell.age}`;
+                ctx.fillText(age, textX, textY);
+            }
+
+            if (gene === null || gene === 1) {
+                const productivity = `P: ${cell.productivity.value.toFixed(3)}`;
+                ctx.fillText(productivity, textX, textY + 15);
+            }
+
+            if (gene === null || gene === 2) {
+                const socialValue = `S: ${cell.socialValue.value.toFixed(3)}`;
+                ctx.fillText(socialValue, textX, textY + 30);
+            }
+        }
+    }
+
+    drawCell = (ctx, cell, x, y, cellWidth, cellHeight) => {
+        let rectFillStyle;
+
+        if (cell.elementType === 0) { //bacterium
+            rectFillStyle = '#00ff00';
+        }
+        else if (cell.elementType === 1){ //virus
+            rectFillStyle = '#ff0000';
         }
 
         ctx.lineWidth = 1;
@@ -250,20 +286,102 @@ class Board extends Component {
 
         ctx.fillStyle = rectFillStyle;
         ctx.fillRect(x, y, cellWidth, cellHeight);
+    }
 
-        if (!this.props.editMode) {
-            const age = `Age: ${cell.age}`;
-            const productivity = `P: ${cell.productivity.value.toFixed(3)}`;
-            const socialValue = `S: ${cell.socialValue.value.toFixed(3)}`;
+    crossoverElements = () => {
+        const canvas = this.board.current;
+        const ctx = canvas.getContext('2d');
 
-            const textX = x  + 6;
-            const textY = y + 15;
+        if (!this.props.generation.cells) return;
+        const { cellWidth, cellHeight } = this.getBoardSize();
 
-            ctx.fillStyle = textFillStyle;
-            ctx.fillText(age, textX, textY);
-            ctx.fillText(productivity, textX, textY + 15);
-            ctx.fillText(socialValue, textX, textY + 30);
+        const elements = [];
+
+        elements.push({dest: { i: 0, j: 0}, firstParent: { i: 1, j: 2 }, secondParent: { i: 1, j: 0 }});
+        elements.push({dest: { i: 1, j: 1}, firstParent: { i: 2, j: 3 }, secondParent: { i: 2, j: 1 }});
+        elements.push({dest: { i: 2, j: 2}, firstParent: { i: 3, j: 2 }, secondParent: { i: 1, j: 2 }});
+        elements.push({dest: { i: 3, j: 3}, firstParent: { i: 4, j: 2 }, secondParent: { i: 1, j: 3 }});
+        elements.push({dest: { i: 4, j: 4}, firstParent: { i: 4, j: 1 }, secondParent: { i: 1, j: 4 }});
+
+        ctx.save();
+        this.crossoverElement(ctx, elements, cellWidth, cellHeight, 1);
+    }
+
+    iniitalizePositionsIfRequired = (cell, cellWidth, cellHeight) => {
+        if (cell.x === undefined || cell.y === undefined) {
+            const { x, y } = this.calculatePosition(cell.i, cell.j, cellWidth, cellHeight);
+            cell.x = x;
+            cell.y = y;
         }
+    }
+
+    setupNextPosition = (cell, dest, step) => {
+        const { x, y } = this.calculateNextPosition(cell.x, cell.y, dest.x, dest.y, step);
+        cell.x = x;
+        cell.y = y;
+    }
+
+    drawParent = (ctx, cell, cellWidth, cellHeight, gene) => {
+        this.drawCell(ctx, this.props.generation.cells[cell.i][cell.j], cell.x, cell.y, cellWidth, cellHeight);
+        this.drawCellValues(ctx, this.props.generation.cells[cell.i][cell.j], cell.x, cell.y, gene);
+    }
+
+    crossoverElement = (ctx, elements, cellWidth, cellHeight) => {
+        const step = 1;
+        let breakCrossover = true;
+
+        this.renderCanvas();
+
+        for (let i = 0; i < elements.length; i++) {
+            const { dest, firstParent, secondParent } = elements[i];
+
+            this.iniitalizePositionsIfRequired(dest, cellWidth, cellHeight);
+            this.iniitalizePositionsIfRequired(firstParent, cellWidth, cellHeight);
+            this.iniitalizePositionsIfRequired(secondParent, cellWidth, cellHeight);
+
+            this.setupNextPosition(firstParent, dest, step);
+            this.setupNextPosition(secondParent, dest, step);
+
+            this.drawParent(ctx, firstParent, cellWidth, cellHeight, 1);
+            this.drawParent(ctx, secondParent, cellWidth, cellHeight, 2);
+
+            if (firstParent.x !== dest.x || firstParent.y !== dest.y || secondParent.x !== dest.x || secondParent.y !== dest.y) {
+                breakCrossover = false;
+            }
+        }
+
+        if (breakCrossover) {
+            return;
+        }
+
+        requestAnimationFrame(() => this.crossoverElement(ctx, elements, cellWidth, cellHeight));
+    }
+
+    calculateNextPosition = (x1, y1, x2, y2, step) => {
+        let x;
+        let y;
+
+        if (x1 < x2) {
+            x = x1 + step;
+        }
+        else if (x1 > x2) {
+            x = x1 - step;
+        }
+        else {
+            x = x1;
+        }
+
+        if (y1 < y2) {
+            y = y1 + step;
+        }
+        else if (y1 > y2) {
+            y = y1 - step;
+        }
+        else {
+            y = y1;
+        }
+
+        return { x, y };
     }
 
     updateState = () => {
@@ -271,7 +389,7 @@ class Board extends Component {
             switch (prevState.currentState) {
                 case 0: {
                     this.renderCanvas();
-                    return { currentState: 1 };
+                    return { currentState: 1, hide: false };
                 }
                 case 1: {
                     this.fadeElements();
@@ -279,7 +397,7 @@ class Board extends Component {
                 }
                 case 2: {
                     this.crossoverElements();
-                    return { currentState: 0 };
+                    return { currentState: 3, hide: true };
                 }
             }
         });
