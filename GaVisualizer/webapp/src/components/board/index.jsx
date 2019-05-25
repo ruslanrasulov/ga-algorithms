91d6142064d5ad7b9aa7a2923e8db1cf99451581
Ruslan Rasulov <ruslanrasulov31@gmail.com>
2019-05-25 16:13:25 +0400

    implemented new board style + fade out on selection

--- GaVisualizer/webapp/src/components/board/index.jsx
+++ GaVisualizer/webapp/src/components/board/index.jsx
@@ -10,7 +10,7 @@ class Board extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { };
+        this.state = { currentState: 0 };
         this.board = React.createRef();
     }
 
@@ -53,8 +53,8 @@ class Board extends Component {
     drawGrid(ctx, cells, boardWidth, boardHeight, cellWidth, cellHeight) {
         ctx.strokeStyle = '#000000';
 
-        for (let i = 0; i < cells.length + 1; i++) {
-            for (let j = 0; j < cells[0].length + 1; j++) {
+        for (let i = 0; i < 6; i++) {
+            for (let j = 0; j < 6; j++) {
                 ctx.beginPath();
                 ctx.moveTo(cellWidth * i, 0);
                 ctx.lineTo(cellWidth * i, boardWidth);
@@ -71,42 +71,49 @@ class Board extends Component {
     fillGrid(ctx, cells, cellWidth, cellHeight) {
         ctx.font = "15px Impact";
 
-        const { generation: { selectedElements } } = this.props;
+        //const { generation: { selectedElements } } = this.props;
 
-        for (let i = 0; i < cells.length; i++) {
-            for (let j = 0; j < cells[0].length; j++) {
-                let rectFillStyle;
-                let textFillStyle;
+        for (let i = 0; i < 5; i++) { //TODO: For temproary purpose
+            for (let j = 0; j < 5; j++) {
+                const { x, y } = this.calculatePosition(i, j, cellWidth, cellHeight);
+                this.drawCell(ctx, cells[i][j], x, y, cellWidth, cellHeight);
+                // let rectFillStyle;
+                // let textFillStyle;
 
-                const selectedCell = selectedElements && selectedElements.find(c => c === cells[i][j].id);
+                // const selectedCell = selectedElements && selectedElements.find(c => c === cells[i][j].id);
 
-                if (selectedCell) {
-                    rectFillStyle = '#ffffff';
-                    textFillStyle = '#000000';
-                }
-                else if (cells[i][j].elementType === 0) { //bacterium
-                    rectFillStyle = '#00ff00';
-                    textFillStyle = '#000000';
-                }
-                else if (cells[i][j].elementType === 1){ //virus
-                    rectFillStyle = '#ff0000';
-                    textFillStyle = '#ffffff';
-                }
+                // if (selectedCell) {
+                //     rectFillStyle = '#ffffff';
+                //     textFillStyle = '#000000';
+                // }
+                // else if (cells[i][j].elementType === 0) { //bacterium
+                //     rectFillStyle = '#00ff00';
+                //     textFillStyle = '#000000';
+                // }
+                // else if (cells[i][j].elementType === 1){ //virus
+                //     rectFillStyle = '#ff0000';
+                //     textFillStyle = '#ffffff';
+                // }
 
-                const x = cellWidth * i;
-                const y = cellHeight * j;
+                // const x = cellWidth * i;
+                // const y = cellHeight * j;
 
-                ctx.fillStyle = rectFillStyle;
-                ctx.fillRect(x, y, cellWidth, cellHeight);
+                // ctx.fillStyle = rectFillStyle;
+                // ctx.fillRect(x, y, cellWidth, cellHeight);
 
-                if (!this.props.editMode) {
-                    const age = cells[i][j].age || 0;
-                    const textX = x  + 6;
-                    const textY = y + 15;
+                // if (!this.props.editMode) {
+                //     const age = `Age: ${cells[i][j].age}`;
+                //     const productivity = `P: ${cells[i][j].productivity.value.toFixed(3)}`;
+                //     const socialValue = `S: ${cells[i][j].socialValue.value.toFixed(3)}`;
 
-                    ctx.fillStyle = textFillStyle;
-                    ctx.fillText(age, textX, textY);
-                }
+                //     const textX = x  + 6;
+                //     const textY = y + 15;
+
+                //     ctx.fillStyle = textFillStyle;
+                //     ctx.fillText(age, textX, textY);
+                //     ctx.fillText(productivity, textX, textY + 15);
+                //     ctx.fillText(socialValue, textX, textY + 30);
+                // }
             }
         }
     }
@@ -122,7 +129,7 @@ class Board extends Component {
         const { cellWidth, cellHeight } = this.getBoardSize();
 
         this.fillGrid(ctx, cells, cellWidth, cellHeight);
-        this.drawGrid(ctx, cells, canvas.width, canvas.height, cellWidth, cellHeight);
+        //this.drawGrid(ctx, cells, canvas.width, canvas.height, cellWidth, cellHeight);
     }
 
     showTooltip = (e) => {
@@ -150,12 +157,12 @@ class Board extends Component {
     }
 
     getBoardSize = () => {
-        const { generation: { cells } } = this.props;
+        //const { generation: { cells } } = this.props;
         const canvas = this.board.current;
 
-        const lineCount = cells.length;
-        const cellWidth = canvas.width / lineCount;
-        const cellHeight = canvas.height / lineCount;
+        //const lineCount = cells.length;
+        const cellWidth = canvas.width / 6;
+        const cellHeight = canvas.height / 6;
 
         return { cellWidth, cellHeight };
     }
@@ -174,8 +181,115 @@ class Board extends Component {
         return cells;
     }
 
+    fadeElements = () => {
+        const canvas = this.board.current;
+        const ctx = canvas.getContext('2d');
+
+        if (!this.props.generation.cells) return;
+        const { cellWidth, cellHeight } = this.getBoardSize();
+
+        for (let i = 0; i < 5; i++) {
+            this.fadeElement(ctx, i, i, cellWidth, cellHeight);
+        }
+    }
+
+    calculatePosition = (i, j, cellWidth, cellHeight) => {
+        let x = 0;
+        let y = 0;
+
+        if (i === 0) {
+            x = 0;
+        }
+        else {
+            x = i * cellWidth + i * 10;
+        }
+
+        if (j === 0) {
+            y = 0;
+        }
+        else {
+            y = j * cellHeight + j * 10;
+        }
+
+        return { x, y };
+    }
+
+    fadeElement(ctx, i, j, cellWidth, cellHeight, step = 0) {
+        const { x, y } = this.calculatePosition(i, j, cellWidth, cellHeight);
+
+        ctx.globalAlpha = 1 - step * 0.01;
+        ctx.fillStyle = '#000000';
+        ctx.clearRect(x - 1, y - 1, cellWidth + 2, cellHeight + 2);
+
+        this.drawCell(ctx, this.props.generation.cells[i][j], x, y, cellWidth, cellHeight);
+
+        if (step === 100) {
+            ctx.restore();
+            return;
+        }
+
+        requestAnimationFrame(() => this.fadeElement(ctx, i, j, cellWidth, cellHeight, step + 1));
+    }
+
+    drawCell = (ctx, cell, x, y, cellWidth, cellHeight) => {
+        let rectFillStyle;
+        let textFillStyle;
+
+        if (cell.elementType === 0) { //bacterium
+            rectFillStyle = '#00ff00';
+            textFillStyle = '#000000';
+        }
+        else if (cell.elementType === 1){ //virus
+            rectFillStyle = '#ff0000';
+            textFillStyle = '#ffffff';
+        }
+
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = '#000000';
+        ctx.strokeRect(x, y, cellWidth, cellHeight);
+
+        ctx.fillStyle = rectFillStyle;
+        ctx.fillRect(x, y, cellWidth, cellHeight);
+
+        if (!this.props.editMode) {
+            const age = `Age: ${cell.age}`;
+            const productivity = `P: ${cell.productivity.value.toFixed(3)}`;
+            const socialValue = `S: ${cell.socialValue.value.toFixed(3)}`;
+
+            const textX = x  + 6;
+            const textY = y + 15;
+
+            ctx.fillStyle = textFillStyle;
+            ctx.fillText(age, textX, textY);
+            ctx.fillText(productivity, textX, textY + 15);
+            ctx.fillText(socialValue, textX, textY + 30);
+        }
+    }
+
+    updateState = () => {
+        this.setState((prevState) => { 
+            switch (prevState.currentState) {
+                case 0: {
+                    this.renderCanvas();
+                    return { currentState: 1 };
+                }
+                case 1: {
+                    this.fadeElements();
+                    return { currentState: 2 };
+                }
+                case 2: {
+                    this.crossoverElements();
+                    return { currentState: 0 };
+                }
+            }
+        });
+    }
+
     render = () => (
-        <canvas className='board' width={400} height={400} ref={this.board} />
+        <div>
+            <canvas className='board' width={480} height={480} ref={this.board} />
+            <button className="btn" onClick={this.updateState}>Next</button>
+        </div>
     )
 }
 
